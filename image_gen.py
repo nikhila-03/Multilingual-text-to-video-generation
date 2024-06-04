06226a9241916d8606f3dc7369c884c1ce78e6f1
Satla Nikhila <131776761+nikhila-03@users.noreply.github.com>
2024-06-04 18:18:08 +0530

    image_gen.py

---
+++ image_gen.py
@@ -0,0 +1,58 @@
+from pygoogle_image import image as pi
+import os
+import shutil
+# import images
+list_checked_names=[]
+def images(final_tokens,name):
+    source_directory = r"C:\Users\Raviteja K\PycharmProjects\opencv1\venv\images"
+
+    # Define a list of image file extensions to consider
+    image_extensions = ('.png', '.jpg', '.jpeg', '.gif', '.bmp')
+
+    # Define the destination directory within the source directory
+    destination_directory = os.path.join(source_directory, name)
+
+    # Create the destination directory (if it doesn't exist)
+    os.makedirs(destination_directory, exist_ok=True)
+
+    for iterator in final_tokens:
+        # print("ldfxhcv nc bddf")
+
+        pi.download(iterator,limit=3)
+        iter = '_'.join(iterator.split(' '))
+        path=r'C:\Users\Raviteja K\PycharmProjects\opencv1\venv\images'
+        path=os.path.join(path,iter)
+        files = os.listdir(path)
+        image_files = [file for file in files if file.lower().endswith(image_extensions)]
+        image_files.sort()
+        if len(image_files) >= 2:
+            for i in range(2):
+                file_to_delete = os.path.join(path, image_files[i])
+                os.remove(file_to_delete)
+    for root, dirs, files in os.walk(source_directory):
+            for file in files:
+                if file.lower().endswith(image_extensions):
+                    # Check if the file is an image
+                    source_file_path = os.path.join(root, file)
+
+                    # Move the image to the destination directory
+                    destination_file_path = os.path.join(destination_directory, file)
+                    if root not in list_checked_names:
+                        shutil.move(source_file_path, destination_file_path)
+    list_checked_names.append(destination_directory)
+    # Delete the remaining empty subdirectories within the source directory
+    # for root, dirs, files in os.walk(source_directory):
+    #     for directory in dirs:
+    #         if directory != name:
+    #             directory_path = os.path.join(root, directory)
+    #             os.rmdir(directory_path)
+
+    # print(list_checked_names)
+    # s3 = boto3.resource('s3')
+    # bucket_name = 'storedimag'
+    # # for loop to iterate and set some name to image
+    # object_key = 'images/image.jpg'
+    # s3.Bucket(bucket_name).upload_file('set some path', object_key)
+
+# tokens=['Barack Obama', 'Martin Luther King Jr.', 'social discrimination', 'illiteracy', 'Nelson Mandela', 'Mahatma Gandhi', 'women empowerment', 'untouchability', 'Gandhi', 'politicians']
+# images(tokens,"hello")
